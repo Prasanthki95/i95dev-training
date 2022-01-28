bc3424ce0c94947e6fa8bfaeb48876126207df8b
prasanthki95 <prasanth.kumar@i95dev.com>
2022-01-28 18:51:18 +0530

    New files added

---
+++ Magento 2/app/code/I95dev/HelloWorld2/Block/Helloworld.php
@@ -0,0 +1,10 @@
+<?php
+namespace I95dev\HelloWorld2\Block;
+ 
+class Helloworld extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'Hello world!';
+    }
+}
---
+++ Magento 2/app/code/Sample/SampleModule/registration.php
@@ -0,0 +1,7 @@
+<?php
+ 
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Sample_SampleModule',
+    __DIR__
+);

1581eddf4e4604c56cd9fc18ffe24a32a0ae4037
prasanthki95 <prasanth.kumar@i95dev.com>
2022-02-10 18:14:11 +0530

    New files updated

--- Magento 2/app/etc/config.php
+++ Magento 2/app/etc/config.php
@@ -351,8 +351,6 @@ return [
         'Dotdigitalgroup_Sms' => 1,
         'Hiddentechies_Base' => 1,
         'Hiddentechies_Bizkick' => 1,
-        'I95Dev_EventsTraining' => 1,
-        'I95Dev_PluginTraining' => 1,
         'I95Dev_ZolutiaShipment' => 1,
         'I95dev_HelloWorld' => 1,
         'I95dev_HelloWorld2' => 1,

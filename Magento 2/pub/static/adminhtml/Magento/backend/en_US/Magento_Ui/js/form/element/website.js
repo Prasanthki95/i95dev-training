f34d066453e587ce6f9bf90d63702dd6de7f42be
prasanthki95 <prasanth.kumar@i95dev.com>
2022-02-22 19:08:30 +0530

    New files updated

--- Magento 2/pub/static/adminhtml/Magento/backend/en_US/Magento_Ui/js/form/element/website.js
+++ Magento 2/pub/static/adminhtml/Magento/backend/en_US/Magento_Ui/js/form/element/website.js
@@ -1,1 +1,32 @@
-/var/www/html/243magento/vendor/magento/module-ui/view/base/web/js/form/element/website.js
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
+ * @api
+ */
+define([
+    'underscore',
+    'uiRegistry',
+    './select'
+], function (_, registry, Select) {
+    'use strict';
+
+    return Select.extend({
+        defaults: {
+            customerId: null,
+            isGlobalScope: 0
+        },
+
+        /**
+         * Website component constructor.
+         * @returns {exports}
+         */
+        initialize: function () {
+            this._super();
+
+            return this;
+        }
+    });
+});
